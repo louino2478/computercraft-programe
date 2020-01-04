077e2a3c7b77eba406863b8168d294067f9724c0
BuildTools <unconfigured@null.spigotmc.org>
2020-01-04 12:25:35 +0100

    modification du readme + deplacement des programe dans le dosier extramods V2
    modification du readme
    deplacement des fichier dans le dossier extramods V2

--- README.md
+++ README.md
@@ -2,19 +2,4 @@
 ## mes programe computercraft sur extramods
 
 
-
-## INDEX
-- reacteur-control.lua
-pour controler le reacteur de mekanism depuis un ecran
-
-- energie-monitor.lua
-pour voir la conso direct des joueur sur un reseau electrique
-
-- energie-compteur.lua
-pour voir la conso totale sur un reseau electrique
-
-- energie-graf-output.lua
-pour avoir un grafique du output du reseau
-
-- energie-graf-input.lua
-pour avoir un grafique du input du reseau
+dans chaaque fichier se trouve les programe fait sur les direrent serveur
--- extramods V3/energie-monitor.lua
+++ extramods V3/energie-monitor.lua
@@ -1,38 +1,26 @@
 m = peripheral.wrap("monitor_1")
-a = peripheral.wrap("Induction Matrix_1")--13roro
-b = peripheral.wrap("Induction Matrix_3")--louloc
-c = peripheral.wrap("Induction Matrix_2")--louino
-f = peripheral.wrap("Induction Matrix_4")--latempete
-g = peripheral.wrap("Induction Matrix_5")--alexxpro
-h = peripheral.wrap("Induction Matrix_6")--kiesta07
+a = peripheral.wrap("Induction Matrix_1")--IM1
+b = peripheral.wrap("Induction Matrix_3")--IM2
+c = peripheral.wrap("Induction Matrix_2")--IM3
+f = peripheral.wrap("Induction Matrix_4")--IM4
+g = peripheral.wrap("Induction Matrix_5")--IM5
+h = peripheral.wrap("Induction Matrix_6")--IM6
 d = peripheral.wrap("Induction Matrix_0")--total
 local e
 
-local function decotext(text)
-m.setTextColor(colors.red)
-m.setCursorPos(12,text)
-m.write("DECO         ")
-m.setTextColor(colors.white)
-end
 
 while true do
 m.clear()
 m.setBackgroundColor(colors.black)
 
 
---lecture des donnee de la diskette
-local handle = assert(fs.open("disk2/vars", "r"), "Couldn't load vars")
-local input = handle.readAll()
-handle.close()
-local data = textutils.unserialize(input)
-
---conso joueur
+--conso IM
 m.setTextColor(colors.white)
 m.setCursorPos(1,1)
 m.write("CONSO JOUEUR")
 m.setTextColor(colors.yellow)
 m.setCursorPos(1,2)
-m.write("13roro ")
+m.write("IM1 ")
 m.setTextColor(colors.green)
 e = a.getOutput()/25
 if e == 0 then
@@ -56,11 +44,10 @@ if e > 100000 then
  m.setCursorPos(17,2)
  m.write("mRF/t                  ")
 end
-data.consororo = e/1000000000+data.consororo
 
 m.setTextColor(colors.yellow)
 m.setCursorPos(1,3)
-m.write("louloc")
+m.write("IM2")
 m.setTextColor(colors.green)
 e = b.getOutput()/25
 if e == 0 then
@@ -84,11 +71,10 @@ if e > 100000 then
  m.setCursorPos(17,3)
  m.write("mRF/t                  ")
 end
-data.consolouloc = e/1000000000+data.consolouloc
 
 m.setTextColor(colors.yellow)
 m.setCursorPos(1,4)
-m.write("louino")
+m.write("IM3")
 m.setTextColor(colors.green)
 e = c.getOutput()/25
 if e == 0 then
@@ -112,7 +98,6 @@ if e > 100000 then
  m.setCursorPos(17,4)
  m.write("mRF/t                  ")
 end
-data.consolouino = e/1000000000+data.consolouino
 
 m.setTextColor(colors.yellow)
 m.setCursorPos(1,5)
@@ -140,11 +125,10 @@ if e > 100000 then
  m.setCursorPos(17,5)
  m.write("mRF/t                  ")
 end
-data.consolatempete = e/1000000000+data.consolatempete
 
 m.setTextColor(colors.yellow)
 m.setCursorPos(1,6)
-m.write("alexxpro")
+m.write("IM4")
 m.setTextColor(colors.green)
 e = g.getOutput()/25
 if e == 0 then
@@ -168,11 +152,10 @@ if e > 100000 then
  m.setCursorPos(17,6)
  m.write("mRF/t                  ")
 end
-data.consoalexxpro = e/1000000000+data.consoalexxpro
 
 m.setTextColor(colors.yellow)
 m.setCursorPos(1,7)
-m.write("none")
+m.write("IM5")
 m.setTextColor(colors.green)
 e = h.getOutput()/25
 if e == 0 then
@@ -196,7 +179,6 @@ if e > 100000 then
  m.setCursorPos(17,7)
  m.write("mRF/t                  ")
 end
-data.consonone = e/1000000000+data.consonone
 
 --*******************************************************************
 
@@ -295,16 +277,7 @@ m.write(e)
 m.setCursorPos(28,11)
 m.write("%              ")
 
--- sauvegarde des donner sur la disquette
-local output = textutils.serialize(data)
-local handle = assert(fs.open("disk2/vars", "w"), "Couldn't save vars")
-handle.write(output)
-handle.close()
 
---------- --roro = 2 --louloc = 3 --latempete = 5 --alexxpro = 6 --kiesta07 = 7
-decotext(7)
---decotext(6)
----------
 
 sleep(1)
 end
