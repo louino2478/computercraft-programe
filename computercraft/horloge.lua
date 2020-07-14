dafb349bdb461edd9f928905f3742354b2580b63
louino2478 <50880693+louino2478@users.noreply.github.com>
2020-07-14 22:33:18 +0200

    ajout du mode pluie
    ajout du mode pluie.
    nécessite un capteur de pluie a l’arrière

--- computercraft/horloge.lua
+++ computercraft/horloge.lua
@@ -5,7 +5,11 @@ while true do
   m.setCursorPos(2,2)
   m.write("J")
   m.write(os.day())
-  m.setCursorPos(2,4)
+  m.setCursorPos(2,3)
   m.write(textutils.formatTime(os.time(), true))
+  m.setCursorPos(2,4)
+  if (rs.getInput("back") == true) then
+    m.write("pluie")
+  end
   sleep(0.3)
 end
