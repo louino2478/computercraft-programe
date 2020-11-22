97919c33baa88de94261596c8a0b8313f24bb21b
louino2478 <50880693+louino2478@users.noreply.github.com>
2020-11-22 17:10:45 +0100

    correction de l'affichage du jour

--- computercraft/horloge.lua
+++ computercraft/horloge.lua
@@ -4,7 +4,7 @@ while true do
   m.clear()
   m.setCursorPos(2,2)
   m.write("J")
-  m.write(os.day())
+  m.write(os.day()-1)
   m.setCursorPos(2,3)
   m.write(textutils.formatTime(os.time(), true))
   m.setCursorPos(2,4)
