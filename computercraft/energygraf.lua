66478c2601c5b3a8dbfe5c7ec74083127cb04ca7
louino2478 <50880693+louino2478@users.noreply.github.com>
2020-12-06 15:55:43 +0100

    taille écran automatique + ajout du texte

--- computercraft/energygraf.lua
+++ computercraft/energygraf.lua
@@ -4,48 +4,55 @@ local input = {}
 local output = {}
 local i
 local n
+local sizew, sizeh = m.getSize()
 m.setTextScale(0.5)
 i = 0
-while i < 80 do
+while i < sizew+1 do
   i = i + 1
   input[i] = 0
   output[i] = 0
 end
 while true do
   i = 0
-  while i < 80 do
+  while i < sizew+1 do
     i = i + 1
     input[i] = input[i+1]
     output[i] = output[i+1]
   end
-  input[80] = math.floor(im.getInput() * 11 / im.getTransferCap())
-  output[80] = math.floor(im.getOutput() * 11 / im.getTransferCap())
+  input[sizew+1] = math.floor(im.getInput() * (sizeh/2-1) / im.getTransferCap())
+  output[sizew+1] = math.floor(im.getOutput() * (sizeh/2-1) / im.getTransferCap())
   
   m.setBackgroundColor(colors.black)
   m.clear()
   i = 0
-  while i < 80 do
+  while i < sizew+1 do
     i = i + 1
-    m.setCursorPos(i,13 + input[i])
+    m.setCursorPos(i,(sizeh/2+1) + input[i])
     m.setBackgroundColor(colors.green)
     m.write(" ")
     n = input[i]
     while n ~= 0 do
       n = n - 1
-      m.setCursorPos(i, 13 + n)
+      m.setCursorPos(i, (sizeh/2+1) + n)
       m.setBackgroundColor(colors.green)
       m.write(" ")
     end
-    m.setCursorPos(i, 12 - output[i])
+    m.setCursorPos(i, (sizeh/2) - output[i])
     m.setBackgroundColor(colors.red)
     m.write(" ")
     n = output[i]
     while n ~= 0 do
       n = n - 1
-      m.setCursorPos(i, 12 - n)
+      m.setCursorPos(i, (sizeh/2) - n)
       m.setBackgroundColor(colors.red)
       m.write(" ")
     end
+    m.setCursorPos(1,sizeh/2+1)
+    m.setBackgroundColor(colors.green)
+    m.write("input")
+    m.setCursorPos(1,sizeh/2)
+    m.setBackgroundColor(colors.red)
+    m.write("output")
   end
   sleep(0.5)
 end
